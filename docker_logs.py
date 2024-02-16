463b9911503001cf0e2b0977e6fd72049191e005
TeaRexcellence <131144814+TeaRexcellence@users.noreply.github.com>
2024-02-16 13:11:43 -0800

    better docker output

--- docker_logs.py
+++ docker_logs.py
@@ -1,7 +1,15 @@
 import os
 import subprocess
+from colorama import Fore, Style
 
 def check_docker_logs(docker_id):
     command = ["docker", "logs", "--follow", docker_id]
-    process = subprocess.run(command, capture_output=True, text=True)
-    print(process.stdout)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            print(output.strip().decode('utf-8'), flush=True)
+    rc = process.poll()
+    return rc
