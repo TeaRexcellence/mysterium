79657700b0eca8d535b3ff64deee1b3d5f63ddf0
TeaRexcellence <131144814+TeaRexcellence@users.noreply.github.com>
2024-02-20 21:40:16 -0800

    perfection logs

--- docker_logs.py
+++ docker_logs.py
@@ -29,10 +29,13 @@ def check_docker_logs(SHOW_INF, SHOW_DBG, SHOW_DEBUG, SHOW_ERR, SHOW_WRN, LOG_HI
     for line in container.logs(stream=True, since=since_time):
         line = line.decode()
 
+        # Remove extra spaces
+        line = re.sub(' +', ' ', line)
+
         # Remove timestamp from ERR messages
         if "ERR" in line and SHOW_ERR:
             err_msg = re.sub(r'^.*ERR', 'ERR', line)
-            print(f"\n🐋 {service_name} ~ " + err_msg, end='', flush=True)  # Added newline at the start
+            print(f"\n🐋 {service_name} ~ " + err_msg, end='', flush=True)
             # Reset counters
             inf_count = 0
             dbg_count = 0
@@ -40,25 +43,48 @@ def check_docker_logs(SHOW_INF, SHOW_DBG, SHOW_DEBUG, SHOW_ERR, SHOW_WRN, LOG_HI
             wrn_count = 0
             # Reset log line
             log_line = f"🐋 {service_name} ~ "
-            continue
         else:
             # Update counters and print counts
             if CONCATENATE_WARNINGS:
                 if "INF" in line and SHOW_INF:
                     inf_count += 1
-                    log_line = log_line.rsplit('INF(', 1)[0] + f"INF({inf_count}),"
+                    if last_log_type != 'INF':
+                        dbg_count = 0
+                        debug_count = 0
+                        wrn_count = 0
+                        log_line += f"INF({inf_count}),"
+                    else:
+                        log_line = log_line.rsplit('INF(', 1)[0] + f"INF({inf_count}),"
                     last_log_type = 'INF'
                 elif "DBG" in line and SHOW_DBG:
                     dbg_count += 1
-                    log_line = log_line.rsplit('DBG(', 1)[0] + f"DBG({dbg_count}),"
+                    if last_log_type != 'DBG':
+                        inf_count = 0
+                        debug_count = 0
+                        wrn_count = 0
+                        log_line += f"DBG({dbg_count}),"
+                    else:
+                        log_line = log_line.rsplit('DBG(', 1)[0] + f"DBG({dbg_count}),"
                     last_log_type = 'DBG'
                 elif line.startswith("DEBUG:") and SHOW_DEBUG:
                     debug_count += 1
-                    log_line = log_line.rsplit('DEBUG(', 1)[0] + f"DEBUG({debug_count}),"
+                    if last_log_type != 'DEBUG':
+                        inf_count = 0
+                        dbg_count = 0
+                        wrn_count = 0
+                        log_line += f"DEBUG({debug_count}),"
+                    else:
+                        log_line = log_line.rsplit('DEBUG(', 1)[0] + f"DEBUG({debug_count}),"
                     last_log_type = 'DEBUG'
                 elif "WRN" in line and SHOW_WRN:
                     wrn_count += 1
-                    log_line = log_line.rsplit('WRN(', 1)[0] + f"WRN({wrn_count}),"
+                    if last_log_type != 'WRN':
+                        inf_count = 0
+                        dbg_count = 0
+                        debug_count = 0
+                        log_line += f"WRN({wrn_count}),"
+                    else:
+                        log_line = log_line.rsplit('WRN(', 1)[0] + f"WRN({wrn_count}),"
                     last_log_type = 'WRN'
             else:
                 if ("INF" in line and SHOW_INF) or \
